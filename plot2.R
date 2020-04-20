754f7ade8e6b2635ff3abd921d3e581b84c1f1df
ranshul191 <rahuljha.jha@gmail.com>
2020-04-20 08:59:10 +0530

    code updated to png size 480

--- plot2.R
+++ plot2.R
@@ -1,17 +1,17 @@
 ##read data
-data<- read.table("household_power_consumption.txt", sep =";", header = TRUE, na.strings = "?", stringsAsFactors = FALSE)
-
-## convert date and time formats
-data$Time <- as.POSIXct(paste(data$Date, data$Time), format = "%d/%m/%Y %H:%M:%S")
-data$Date <-as.Date(data$Date, format = "%d/%m/%Y")
-
+Power_data<- read.table("household_power_consumption.txt", sep =";", header = TRUE, na.strings = "?", stringsAsFactors = FALSE)
 
 ##create subdata for further use
-subdata <- subset(data, Date>="2007-02-01" & Date <="2007-02-02")
+subPower_data <- subset(Power_data, Date=="1/2/2007"|Date=="2/2/2007")
+
+## convert date and time formats
+subPower_data$Time <- as.POSIXct(paste(subPower_data$Date, subPower_data$Time), format = "%d/%m/%Y %H:%M:%S")
+subPower_data$Date <-as.Date(subPower_data$Date, format = "%d/%m/%Y")
+
 
 ##create a png file 
-png("plot2.png", height = 400, width = 400)
+png("plot2.png", height = 480, width = 480)
 
 ##create plot
-plot(subdata$Time, subdata$Global_active_power, ylab = "Global Active Power (kilowatts)", xlab="", type="l")
+plot(subPower_data$Time, subPower_data$Global_active_power, ylab = "Global Active Power (kilowatts)", xlab="", type="l")
 dev.off()
